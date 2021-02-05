45fbb6c76fbae1e3b32787f022d28306790bc009
mauroxcf <maurox03@gmail.com>
2021-02-05 14:26:02 -0500

    Mastermind v.1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const list_colors = ['red', 'blue', 'aqua', 'yellow', 'orange', 'white'];
-let listToWin = [];
+let listToWin = randomColors();;
 let current_attemp = 0;
 
 let mainMatrix = [
@@ -15,10 +15,8 @@ let mainMatrix = [
 window.onload = function () {
     for (let i = 0; i < 7; i++) {
         addElement(i);
-        allColors(i)
+        addBtnClues(i)
     }
-    listToWin = randomColors();
-    console.log(getCurrentPieces());
 }
 
 function addElement(index) {
@@ -47,7 +45,7 @@ function createsSubDivs(index, clsName) {
     return div;
 }
 
-function allColors(index) {
+function addBtnClues(index) {
     let cluesDiv = document.getElementById('div_clues');
 
     let colorsDiv = document.createElement('div');
@@ -55,10 +53,48 @@ function allColors(index) {
     colorsDiv.className = 'attemp_clues';
 
     for (let i = 0; i <= 3; i++) {
-        colorsDiv.appendChild(createsSubDivs(i, 'attemps'));
+        colorsDiv.appendChild(createsSubDivs(i, 'clue_attemps'));
     }
     cluesDiv.appendChild(colorsDiv);
 }
+
+
+function checker() {
+    let pieces = getCurrentPieces();
+    let pieceColors = mainMatrix[current_attemp];
+    let len = listToWin.length;
+    let list_clues = [0, 0, 0, 0];
+
+    if (pieceColors.includes('')){
+        alert("LLenelo")
+        return
+    }
+    console.log(listToWin);
+    console.log(pieceColors);
+
+    for (let i = 0; i < len; i++) {
+        if (pieceColors[i] == listToWin[i]) {
+            list_clues[i] = 2;
+        }
+    }
+
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (pieceColors[i] == listToWin[j] && list_clues[i] == 0) {
+                if (list_clues[j] == 0)
+                {
+                    list_clues[i] = 1;
+                }
+            }
+        }
+    }
+
+    colorCheck(list_clues);
+    /* block the current array */
+    /* check if win or if lose*/
+    current_attemp++;
+}
+
 
 function puttingColor (element) {
     let currentPieces = getCurrentPieces();
@@ -85,11 +121,11 @@ function colorCheck (listaDeNumeros) {
     let temp = getBtnClue();
     for (let chek = 0; chek < listaDeNumeros.length; chek++) {
         if (listaDeNumeros[chek] == 1) {
-            temp[chek].className = 'attemps clue_white';
+            temp[chek].className = 'clue_attemps clue_white';
 
         }
         if (listaDeNumeros[chek] == 2) {
-            temp[chek].className = 'attemps clue_red';
+            temp[chek].className = 'clue_attemps clue_red';
 
         }
     }
@@ -97,7 +133,7 @@ function colorCheck (listaDeNumeros) {
 
 function getBtnClue() {
     var x = document.getElementById("attemp_clue" + current_attemp);
-    var subDivs = x.getElementsByClassName("attemps");
+    var subDivs = x.getElementsByClassName("clue_attemps");
     let color_list = [];
 
     for (let i = 0; i < 4; i++) {

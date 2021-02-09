3f4c98b56238fc2b534dea8317445f7ff25e7eaa
mauroxcf <maurox03@gmail.com>
2021-02-09 00:06:27 -0500

    adding documentation in every function

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const list_colors = ['red', 'blue', 'aqua', 'yellow', 'orange', 'white'];
 let listToWin = randomColors();
+
+/* the current attemp of the player, increase whit every attemp*/
 let current_attemp = 0;
 
+/* the rows of tries that can make the player
+each array is an attemp
+*/
 let mainMatrix = [
     ['', '', '', ''],
     ['', '', '', ''],
@@ -12,6 +17,10 @@ let mainMatrix = [
     ['', '', '', '']
 ]
 
+/**
+ * onload - the inicializator
+ * this function runs after HTML is completely load
+ */
 window.onload = function () {
     for (let i = 0; i < 7; i++) {
         addElement(i);
@@ -19,11 +28,17 @@ window.onload = function () {
     }
     let hgh = window.screen.availHeight;
     let wth = window.screen.availWidth;
+    /* verify the height and width of the screen */
     if (hgh < 760 || hgh < 1260) {
         alert("Please zoom out of your browser for a better experience.");
     }
 }
 
+/**
+ * addElement - creates and adds the divs attemps of the player
+ *
+ * @param  {Int} index - the number for the unique id of each div attempt
+ */
 function addElement(index) {
     let contentDiv = document.getElementById('div_content_attempts');
     let parent_div = document.createElement('div');
@@ -41,6 +56,12 @@ function addElement(index) {
     contentDiv.appendChild(parent_div);
 }
 
+/**
+ * createsSubDivs - creates a new div
+ *
+ * @param  {Int} index - the number for the unique id of each div attempt
+ * @param  {String} clsName - the class name for the new div
+ */
 function createsSubDivs(index, clsName) {
     let div = document.createElement('div');
 
@@ -50,6 +71,11 @@ function createsSubDivs(index, clsName) {
     return div;
 }
 
+/**
+ * addBtnClues - creates and adds the divs to show the clues
+ *
+ * @param  {} index - the number for the unique id of each div attempt
+ */
 function addBtnClues(index) {
     let cluesDiv = document.getElementById('div_clues');
 
@@ -63,11 +89,25 @@ function addBtnClues(index) {
     cluesDiv.appendChild(colorsDiv);
 }
 
-
+/**
+ * checker - generates a list with the hints if the attempt it's accurate
+ * if the color is correct and it's in the correct position,
+ * the value in the list will be 2
+ *
+ * if the color only is correct the value will be 1
+ *
+ * and 0 otherwise
+ */
 function checker() {
     let pieces = getCurrentPieces();
     let pieceColors = mainMatrix[current_attemp];
+
+    /*
+    * Copy of the array with the code to guess
+    * because this array will be modified
+    */
     let copyListToWin = listToWin.slice(0);
+
     let len = copyListToWin.length;
     let list_clues = [0, 0, 0, 0];
 
@@ -76,6 +116,11 @@ function checker() {
         return
     }
 
+    /*
+    * This loop first find the correct color in the correct position
+    * and deletes if there is a coincidence
+    * the red clue in the game
+    */
     for (let i = 0; i < len; i++) {
         if (pieceColors[i] == copyListToWin[i]) {
             list_clues[i] = 2;
@@ -83,6 +128,11 @@ function checker() {
         }
     }
 
+    /*
+    * and this loop it's to find the correct color
+    * but in the incorrect position
+    * the white clue in the game
+    */
     for (let i = 0; i < len; i++) {
         for (let j = 0; j < len; j++) {
             if (pieceColors[i] == copyListToWin[j] && list_clues[i] == 0) {
@@ -102,6 +152,9 @@ function checker() {
     win_or_lose(list_clues);
 }
 
+/**
+ * dontGoBack - blocks the attemps after the check.
+ */
 function dontGoBack() {
     var x = document.getElementById("div_attemp" + current_attemp);
     let color_list = getCurrentPieces();
@@ -113,7 +166,13 @@ function dontGoBack() {
     x.style = "opacity: 50%;"
 }
 
-
+/**
+ * win_or_lose - call a corresponding function to show
+ * if the player win or loose the game with a message.
+ *
+ * @param  {Array} list_clues - contain the code to change color to the clues
+ * in the div_clues
+ */
 function win_or_lose(list_clues) {
     let i;
     for (i = 0; i < list_clues.length; i++) {
@@ -132,6 +191,12 @@ function win_or_lose(list_clues) {
     }
 }
 
+/**
+ * endMessage - unlock the correct code combination and
+ *block the screen to show if the user win o loose the game
+ *
+ * @param  {Int} win - number that indicate if the user win
+ */
 function endMessage(win) {
     let showDiv = document.getElementById('div_end');
     let message = document.getElementById('result_message');
@@ -151,7 +216,12 @@ function endMessage(win) {
 }
 
 
-
+/**
+ * puttingColor - insert a color in each attemp of the row
+ *
+ * @param  {HTMLElement} element - the div that contains the color selected
+ * in her className
+ */
 function puttingColor (element) {
     let currentPieces = getCurrentPieces();
     let get_color = getClassColor(element);
@@ -173,8 +243,14 @@ function puttingColor (element) {
     indexM[color_index] = get_color
 }
 
+/**
+ * colorCheck - show the hints in white or red of the row
+ *
+ * @param {Array} numberList - is the listed code to put the color clues
+ */
 function colorCheck (numberList) {
     let temp = getBtnClue();
+
     for (let chek = 0; chek < numberList.length; chek++) {
         if (numberList[chek] == 1) {
             temp[chek].className = 'clue_attemps clue_white';
@@ -185,6 +261,9 @@ function colorCheck (numberList) {
     }
 }
 
+/**
+ * getBtnClue - returns the divs where will be shown the clue color
+ */
 function getBtnClue() {
     var x = document.getElementById("attemp_clue" + current_attemp);
     var subDivs = x.getElementsByClassName("clue_attemps");
@@ -197,6 +276,9 @@ function getBtnClue() {
     return color_list;
 }
 
+/**
+ * randomColors - returns the list with the code to guess by the player
+ */
 function randomColors() {
     const arr = new Array();
 
@@ -208,7 +290,9 @@ function randomColors() {
     return arr;
 }
 
-
+/**
+ * getCurrentPieces - get the current row attempt
+ */
 function getCurrentPieces() {
     var x = document.getElementById("div_attemp" + current_attemp);
     var subDivs = x.getElementsByClassName("btn_pieces");
@@ -221,6 +305,10 @@ function getCurrentPieces() {
     return color_list;
 }
 
+/**
+ * getClassColor - obtain the color of the class
+ * @param {HTMLElement} element - the div that contains the color selected
+ */
 function getClassColor(element) {
     let res = element.className.split(" ");
     let color = res[res.length - 1]
@@ -230,7 +318,10 @@ function getClassColor(element) {
     }
     return null
 }
-
+/**
+ * eraseColor - delete a previus selection attemp of the player
+ * @param {HTMLElement} element - the div that contains the color selected
+ */
 function eraseColor(element) {
     let res = element.className.split(" ");
     let valueColor = res[1];
@@ -241,6 +332,9 @@ function eraseColor(element) {
     mainMatrix[current_attemp][ValueIndex] = "";
 }
 
+/**
+ * showInfo - is to show the div with the game rules
+ */
 let countInfo = 0;
 function showInfo() {
     let x = document.getElementById("rules_info");
